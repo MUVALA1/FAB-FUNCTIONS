6111da4b3e7eda5442d531c67fd9ea854f6b8c90
nyiramirembe <peninahhot@gmail.com>
2019-07-02 18:01:05 +0300

    work

---
+++ fab.js
@@ -0,0 +1,11 @@
+function openmenu(){
+	document.getElementsById("side-menu").style.display="block";
+	document.getElementsById("menu-btn").style.display="none";
+	document.getElementsById("close-btn").style.display="block";
+}
+function closemenu(){
+	document.getElementsById("side-menu").style.display="none";
+	document.getElementsById("menu-btn").style.display="block";
+	document.getElementsById("close-btn").style.display="none";
+}
+
